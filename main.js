0d75e58388ec6ecf7daaae1619f47325a39de204
CallensJ <callens.johan@protonmail.com>
2024-10-17 13:31:37 +0200

    add dropdown javascript onClick event

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 import './sass/main.scss'
 
 
+const dropdownLinks = document.querySelectorAll('.dropdown__link');
+const allDropdownMenus = document.querySelectorAll('.dropdown__menu');
+
+dropdownLinks.forEach(link => {
+  link.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const dropdownMenu = link.nextElementSibling;
+  
+    allDropdownMenus.forEach(menu => {
+      if (menu !== dropdownMenu) {
+        menu.classList.remove('show');
+      }
+    });
+    dropdownMenu.classList.toggle('show');
+  })
+})
+
+
