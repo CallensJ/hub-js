7d651a8dfb3603b74e988e1f00acd8b206a416b9
CallensJ <callens.johan@protonmail.com>
2024-10-18 11:06:19 +0200

    added cours-1.html

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-import './sass/main.scss'
+import "./sass/main.scss";
 
+const dropdownLinks = document.querySelectorAll(".dropdown__link");
+const allDropdownMenus = document.querySelectorAll(".dropdown__menu");
 
-const dropdownLinks = document.querySelectorAll('.dropdown__link');
-const allDropdownMenus = document.querySelectorAll('.dropdown__menu');
-
-dropdownLinks.forEach(link => {
-  link.addEventListener('click', (e) =>{
+dropdownLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
     const dropdownMenu = link.nextElementSibling;
-  
-    allDropdownMenus.forEach(menu => {
+
+    allDropdownMenus.forEach((menu) => {
       if (menu !== dropdownMenu) {
-        menu.classList.remove('show');
+        menu.classList.remove("show");
       }
     });
-    dropdownMenu.classList.toggle('show');
-  })
-})
-
-
+    dropdownMenu.classList.toggle("show");
+  });
+});
